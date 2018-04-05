697d5b7fc81be74d22912c7b48a5a3486e6a51f3
solisgb <37953927+solisgb@users.noreply.github.com>
2018-04-05 21:47:38 +0200

    Add files via upload

--- IDW.py
+++ IDW.py
@@ -5,23 +5,28 @@ Created on Sun Mar 25 17:58:36 2018
 @author: solis
 """
 
+import numpy as np
 import pyodbc
 
 
 def rellenar_huecos():
     """
-    hago una conexión a la BD
-    busco los puntos sin dato, ordenados por fechas
-    para cada fecha paso a interpola la fecha, los codigos de los puntos
-    sin datos cod y sus coordenadas
+    1. hago una conexión a la BD
+    2. busco los puntos sin dato, ordenados por fechas
+    3. para cada fecha busco todas las estaciones sin dato en esa fecha y
+       cojo su codigo y coordenadas
+    4. cuando cambia la fecha llamo a la función data_in_fecha
+    ________________________________
+    cod. lista de códigos de estaciones sin dato en fecha
+    xy. lista de coordenadas de cada estación sin dato
     len(cod)=len(xy)
-    len(xy[0])=2
+    xy.shape=[len(cod), 2]
     """
 
-    import connexion_str as cstr
+    import db_con_str
     import IDW_parameters as par
 
-    a = cstr.conexion_str_get(par.DB)
+    a = db_con_str.con_str(par.DB)
     con = pyodbc.connect(a)
     cur = con.cursor()
     cur.execute(par.select_v)
@@ -36,53 +41,86 @@ def rellenar_huecos():
 
         if fecha == row.FECHA:
             cod.append(row.ID)
-            xy.append([row.C_X, row.C_Y])
+            xy.append([[row.X, row.Y]])
         else:
-            interpola(fecha, cod, xy)
+            data_in_fecha(con, fecha, cod, np.array(xy, dtype=np.float32)
             cod = [row.ID]
-            xy = [[row.C_X, row.C_Y]]
+            xy = [[row.X, row.Y]]
 
     cur.close()
     con.close()
 
 
-def interpola(con, fecha, cod, xy):
+def data_in_fecha(con, fecha, codv, xyv):
     """
-    para la fecha hay len(cod) puntos de coordenadas xy = [[x1, y1]...]
+    para la fecha hay len(codv) puntos de coordenadas xyv = [[x1, y1], ...]
     sin datos
-    se seleccionan en con los puntos con datos para fecha
-    se forma un kdtree con los puntos con datos
-    se llama a idw para calcular los valores de los puntos sin datos
+    1. se ejecuta un select que devuelve los puntos con datos para fecha
+    2. se forma un kdtree con los puntos con datos
+    3. se ejecuta un kdtree query pata ordenar a distancias dist crecientes 
+       cada punto sin datos con respecto a los puntos con datos,también 
+       devuelve la lista de los índices ii, de modo que se puede identificar
+       cada punto con datos
+    4. se llama a la función data_in_quadrant
     """
-    import numpy as np
     from scipy import spatial
     import IDW_parameters as par
     cur = con.cursor()
     cur.execute(par.select_d)
     rows = [row for row in cur]
     cur.close()
-    xd = np.array([row[0] for row in rows], dtype=np.float32)
-    yd = np.array([row[1] for row in rows], dtype=np.float32)
-    tree = spatial.KDTree(xd, yd)
-    n = len(xy)
-    d, ii = tree.query(xy, k=n)
-    v_interpolated = idw(d, ii, par.power)
+    xyd = np.array([[row.X, row.Y] for row in rows], dtype=np.float32)
+    # the name of the 3rd element varies in each select
+    z = np.array([row[2] for row in rows], dtype=np.float32)
+    tree = spatial.cKDTree(xyd)
+    n = len(xyv)
+    dist, ii = tree.query(xyv, k=n)
+    data_in_quadrant(xyv, xyd, z, dist, ii)
 
-    def getQuadrant(xs, ys):
-        """
-        dados dos np.array xs ys con igual numero de elementos
-        xs contiene las coordenadas x de una serie de puntos
-        ys contiene las coordenadas y de la misma serie de puntos
-        devuelve el cuadrante de capa punto
-        """
-        assert xs.size==ys.size, 'xy ys must have equal size'
-        bxs=xs>=0
-        bys=ys>=0
-        ixs=np.zeros(len(bxs), np.int32)
-        iys=np.zeros(len(bys), np.int32)
-        ixs[:]=bxs[:]
-        iys[:]=bys[:]
-        return 3 + ixs - iys - 2 * ixs * iys
+
+def data_in_quadrant(xyv, xyd, data, d, ii):
+    """
+    para cada punto sin dato de coordenadas xyv1 selecciona los 4 puntos más
+    próximos en cada cuadrante
+    1. Para cada xyv1 hace una traslación de coordenadas de modo que xyv1
+       se quede en el centro de coornadas -> xt, yt
+    2. Calcula el cuadrante de los puntos con datos llamando a la función
+       get_quadrant
+    """
+    dist_selected = []
+    data_selected = []
+    q1p = np.zeros((4), dtype=np.int32)
+    for xyv1 in xyv:
+        xt = xyd[:,0] - xyv1[:,0]
+        yt = xyd[:,1] - xyv1[:,1]
+        iq = get_quadrant(xt, yt)
+        d1p = []
+        for i in range(len(iq)):
+            if q1p[iq[i]-1] == 0:
+                q1p[iq[i]-1] = 1
+                d1p.append() = d[0]
+                if np.sum(q1p) == 4:
+                    d_selected.append(d1p)
+                    q_data_selected.append(q1p)
+                    break
+        if np.sum(q1p) < 4:
+            d_selected.append(d1p)
+            q_data.append(q1p)
+    return
+
+
+def get_quadrant(xs, ys):
+    """
+    dados dos np.array xs ys con igual numero de elementos
+    xs contiene las coordenadas x de una serie de puntos
+    ys contiene las coordenadas y de la misma serie de puntos
+    devuelve el cuadrante de capa punto
+    """
+    bxs = xs >= 0
+    bys = ys >= 0
+    ixs = bxs.astype(np.int32)
+    iys = bys.astype(np.int32)
+    return 3 + ixs - iys - 2 * ixs * iys
 
 
 def idw(d, ii, power):
--- IDW_parameters.py
+++ IDW_parameters.py
@@ -6,30 +6,26 @@ Created on Sun Mar 25 17:52:37 2018
 parámetros del script
 """
 
+# potencia a la que se eleva el método idw
 power = 2
 
 # bases de datos de datos y huecos
-# DB = r'E:\WORK\CHS\aemet\out\AEMET_CHS.accdb'
-DB = r'C:\Users\solis\Documents\aemet\DB\AEMET_CHS.accdb'
+DB = r'E:\WORK\CHS\aemet\out\AEMET_CHS.accdb'
+# DB = r'C:\Users\solis\Documents\aemet\DB\AEMET_CHS.accdb'
 
-# relleno de huevos de P
-# select que busca en DB_VOIDS las estaciones y fechas sin datos
-select_v = """SELECT Estaciones.C_X, Estaciones.C_Y, P_voids.ID, P_voids.FECHA
+# voids in P series: select que busca en DB las estaciones y fechas sin datos
+select_v = """SELECT Estaciones.X, Estaciones.Y, P_voids.ID, P_voids.FECHA
     FROM Estaciones INNER JOIN P_voids ON Estaciones.ID = P_voids.ID
     WHERE P_voids.FECHA=#1/1/1965#
     ORDER BY P_voids.FECHA"""
 
-select_d = """SELECT Estaciones.C_X, Estaciones.C_Y, P.P
+# data in P series: select to retrieve data for each fecha
+select_d = """SELECT Estaciones.X, Estaciones.Y, P.P
     FROM Estaciones INNER JOIN P ON Estaciones.ID = P.ID
     WHERE FECHA=?"""
 
-dir_data = r'E:\WORK\CHS\aemet\out'
+# directorio de resultados
+dir_out = r'E:\WORK\CHS\aemet\out'
 
-# fichero de huecos de cada estacion (ID, FECHA)
-# debe estar ordenado por fechas
-fvoids = 'p_voids.txt'
-
-# directorio de datos
-dir_out = dir_data
-
-fout = 'p_interpolated.txt'
+# P results: name file
+fpout = 'p_idw.txt'
